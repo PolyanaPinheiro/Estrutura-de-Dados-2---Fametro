e3c979ea515e5fcf6d031995146da293528f0d5f
PolyanaPinheiro <polyanaferreira1236@gmail.com>
2025-10-19 19:44:05 -0400

    Update questao1.c

--- Lista de Exercicio 3 - ED3/questao1.c
+++ Lista de Exercicio 3 - ED3/questao1.c
@@ -1,3 +1,14 @@
+/* 1) Dado duas pilhas P1 e P2 que armazenam números inteiros, faça um programa que tenha as
+seguintes funções:
+ Uma função para testar se as duas pilhas P1 e P2 são iguais. Caso não forem iguais diga
+qual pilha é maior
+ Uma função que forneça o maior, o menor e a média aritmética dos elementos de uma
+pilha.
+ Uma função para transferir elementos de P1 para P2 (copia).
+ Uma função para retornar o número de elementos de uma pilha que possuem valor
+ımpar.
+ Uma função para retornar o número de elementos da uma pilha que possuem valor par */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -209,3 +220,4 @@ int main() {
 
     return 0;
 }
+
